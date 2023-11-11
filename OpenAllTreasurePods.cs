fa844a4bc3c9f3174691d753fa9ba83725eea850
Mark Kraus <github@markekraus.com>
2023-11-11 10:01:24 -0800

    Initial Release

---
+++ OpenAllTreasurePods.cs
@@ -0,0 +1,21 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+namespace OpenAllTreasurePods
+{
+    internal class Entry : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "zoneCore" || !sceneName.Contains("zone"))
+                return;
+
+            foreach (var item in Resources.FindObjectsOfTypeAll<TreasurePod>())
+            {
+                if (item.CurrState == TreasurePod.State.LOCKED)
+                    item.Activate();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using OpenAllTreasurePods;
+using MelonLoader;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: MelonAuthorColor(255, 255, 0, 188)]
+[assembly: MelonColor(255, 100, 134, 196)]
+[assembly: MelonInfo(typeof(Entry), "OpenAllTreasurePods", "1.0.0", "MarkEKraus", null)]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
+[assembly: AssemblyTitle("OpenAllTreasurePods")]
+[assembly: AssemblyDescription("Slime Rancher 2 mod that automatically opens all treasure pods.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mark E. Kraus")]
+[assembly: AssemblyProduct("OpenAllTreasurePods")]
+[assembly: AssemblyCopyright("Copyright © Mark E. Kraus 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1348b6b2-5aa5-4b3b-8f8b-acdbcf6cc00d")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
