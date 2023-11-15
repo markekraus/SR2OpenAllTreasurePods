f7cf64cea324d7c886c2a4d534b6df6828bc857e
Mark Kraus <github@markekraus.com>
2023-11-15 15:22:41 -0800

    Update for Slime Rancher 2 v0.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 using OpenAllTreasurePods;
 using MelonLoader;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: MelonAuthorColor(255, 255, 0, 188)]
 [assembly: MelonColor(255, 100, 134, 196)]
-[assembly: MelonInfo(typeof(Entry), "OpenAllTreasurePods", "1.0.0", "MarkEKraus", null)]
+[assembly: MelonInfo(typeof(Entry), "OpenAllTreasurePods", "2.0.0", "MarkEKraus", null)]
 [assembly: MelonGame("MonomiPark", "SlimeRancher2")]
 [assembly: AssemblyTitle("OpenAllTreasurePods")]
 [assembly: AssemblyDescription("Slime Rancher 2 mod that automatically opens all treasure pods.")]
@@ -18,5 +18,5 @@ using MelonLoader;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1348b6b2-5aa5-4b3b-8f8b-acdbcf6cc00d")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
